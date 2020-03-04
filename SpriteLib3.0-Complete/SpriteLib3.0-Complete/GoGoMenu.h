3c15a03c732fa7adec9c6992838e01662bfcfbaf
Tyler <tyler.wong@ontariotechu.net>
2020-03-04 12:43:15 -0500

    V2.5.3
    Lerped black and white

--- SpriteLib3.0-Complete/SpriteLib3.0-Complete/GoGoMenu.h
+++ SpriteLib3.0-Complete/SpriteLib3.0-Complete/GoGoMenu.h
@@ -13,9 +13,9 @@ public:
 	void RainbowBackground();
 
 protected:
-	float m_repeatTime = 5.f;
+	float m_repeatTime = 2.f;
 	float m_lerpVal = 0.f;
 
-	vec4 m_clearColor1 = vec4(0.153f, 0.992f, 0.961f, 1.f);
-	vec4 m_clearColor2 = vec4(0.969f, 0.396f, 0.722f, 1.f);
+	vec4 m_clearColor1 = vec4(0.f, 0.f, 0.f, 1.f);//vec4(0.153f, 0.992f, 0.961f, 1.f);
+	vec4 m_clearColor2 = vec4(1.f, 1.f, 1.f, 1.f);//vec4(0.969f, 0.396f, 0.722f, 1.f);
 };
